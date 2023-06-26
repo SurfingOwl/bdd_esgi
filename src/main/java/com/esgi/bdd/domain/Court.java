25160c8253b78d97e6523cbd40d598a148bd6b48
Monel.D <monelyehoun@gmail.com>
2023-06-26 21:39:08 +0200

    Réaction des Scenario

--- src/main/java/com/esgi/bdd/domain/Court.java
+++ src/main/java/com/esgi/bdd/domain/Court.java
@@ -3,4 +3,7 @@ package com.esgi.bdd.domain;
 import lombok.With;
 
 public record Court(@With Player player1, @With Player player2, @With GameStatus gameStatus) {
+    public Object getGameStatus() {
+        return  this.gameStatus;
+    }
 }
--- src/main/java/com/esgi/bdd/domain/Player.java
+++ src/main/java/com/esgi/bdd/domain/Player.java
@@ -3,4 +3,11 @@ package com.esgi.bdd.domain;
 import lombok.With;
 
 public record Player(String name, @With ScoreEnum score, @With boolean won) {
+    public Object getScore() {
+        return this.score;
+    }
+
+    public boolean isWon() {
+        return this.won;
+    }
 }
